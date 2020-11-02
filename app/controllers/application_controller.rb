f443a1c8fb8bd32778f28134efc2b20503e3f7c2
Steph Vallance <44055497+steph603@users.noreply.github.com>
2020-11-02 13:44:56 +1100

    Added username field to users table, sign in form & welcome alert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
 
     before_action :authenticate_user!, only: [:restricted]
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
+    
 
 end
