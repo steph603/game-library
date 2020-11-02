de7bb7eb4724cab8212ca2bb329d18e7d0b82924
Steph Vallance <44055497+steph603@users.noreply.github.com>
2020-11-02 16:23:21 +1100

    Some HTML and formatting

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'pages/home'
   get 'pages/restricted'
+  
   root to: "pages#home"
 
 end
