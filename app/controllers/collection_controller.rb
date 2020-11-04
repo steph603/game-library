dfbb97d060d488e546087b0c2148182326d8a7c4
Steph Vallance <44055497+steph603@users.noreply.github.com>
2020-11-04 11:24:23 +1100

    My collection logic

--- app/controllers/collection_controller.rb
+++ app/controllers/collection_controller.rb
@@ -2,15 +2,23 @@ class CollectionController < ApplicationController
 
     def my_collection
 
-        @games = Collection.all
+        @mygames = Collection.all
         @collection = []
-        @games.each { |game| 
+        @allgames = Game.all
+        @games = []
+        @mygames.each { |game| 
             if game.user_id == current_user.id
-                @collection << game
+                @collection << game.game_id
             end
         }
 
-
+        for id in @collection do 
+            for game in @allgames do 
+                if game.id == id 
+                    @games << game
+                end
+            end
+        end
     end
     
     def collections
